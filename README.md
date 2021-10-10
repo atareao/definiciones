bcdb41c1de79b93820bd5addf299e9809d3229c0
Lorenzo <lorenzo.carbonell.cerezo@gmail.com>
2021-10-10 11:05:21 +0200

    First version

---
+++ README.md
@@ -0,0 +1,100 @@
+<h1 align="center">Welcome to Ubuntu First Steps 👋<h1>
+
+![Licencia MIT](https://img.shields.io/badge/Licencia-MIT-green)
+[![Codacy Badge](https://api.codacy.com/project/badge/Grade/b3e704c3f150404582cd23b9fcb4be32)](https://www.codacy.com/manual/atareao/ubuntu-first-steps?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=atareao/ubuntu-first-steps&amp;utm_campaign=Badge_Grade)
+[![CodeFactor](https://www.codefactor.io/repository/github/atareao/ubuntu-first-steps/badge/master)](https://www.codefactor.io/repository/github/atareao/ubuntu-first-steps/overview/master)
+
+[![Twitter: atareao](https://img.shields.io/twitter/follow/atareao.svg?style=social)](https://twitter.com/atareao)
+
+<img src="./data/icons/scalable/apps/ubuntu-first-steps.svg" align="right"
+     title="start-here Logo" width="128" height="128">
+
+
+## 🏠 [Homepage](https://www.atareao.es/aplicacion/ubuntu-first-steps/)
+
+## Prerequisites
+
+Before you begin, ensure you have met the following requirements:
+
+* If you install it from PPA don't worry about, becouse all the requirements are included in the package
+* If you clone the repository, you need, at least, these dependecies,
+
+```
+python3-gi
+gir1.2-gtk-3.0
+gir1.2-gdkpixbuf-2.0
+gir1.2-glib-2.0
+gir1.2-vte-2.91
+gir1.2-handy-0.0
+```
+
+## Installing Ubuntu First Steps
+
+To install **Ubuntu First Steps**, follow these steps:
+
+* In a terminal (`Ctrl+Alt+T`), run these commands
+
+```
+sudo add-apt-repository ppa:atareao/atareao
+sudo apt update
+sudo apt install ubuntu-first-steps
+```
+
+## Using Ubuntu First Steps
+
+**Ubuntu First Steps** has 5 main sections to custom the Ubuntu User Experience
+
+* Custom dock
+
+![Custom dock](./screenshots/ubuntu-first-steps-screenshot-1.png)
+
+* Custom desktop
+
+![Custom desktop](./screenshots/ubuntu-first-steps-screenshot-2.png)
+
+* Privacy
+
+![Privacy](./screenshots/ubuntu-first-steps-screenshot-3.png)
+
+* Repositories
+
+![Repositorios](./screenshots/ubuntu-first-steps-screenshot-4.png)
+
+* Applications
+
+![Applications](./screenshots/ubuntu-first-steps-screenshot-5.png)
+
+### About this application
+
+![About this app](./screenshots/ubuntu-first-steps-screenshot-6.png)
+
+## Contributing to Ubuntu First Steps
+
+To contribute to **Ubuntu First Steps**, follow these steps:
+
+1. Fork this repository.
+2. Create a branch: `git checkout -b <branch_name>`.
+3. Make your changes and commit them: `git commit -m '<commit_message>'`
+4. Push to the original branch: `git push origin <project_name>/<location>`
+5. Create the pull request.
+
+Alternatively see the GitHub documentation on [creating a pull request](https://help.github.com/en/github/collaborating-with-issues-and-pull-requests/creating-a-pull-request).
+
+## 👤 Contributors ✨
+
+Thanks goes to these wonderful people ([emoji key](https://allcontributors.org/docs/en/emoji-key)):
+
+<table>
+  <tr>
+    <td align="center"><a href="https://www.atareao.es"><img src="https://avatars3.githubusercontent.com/u/298055?v=4" width="100px;" alt=""/><br /><sub><b>Lorenzo Carbonell</b></sub></a><br /><a href="https://github.com/atareao/fondos-productivos/commits?author=atareao" title="Code">💻</a></td>
+  </tr>
+</table>
+
+
+## Contact
+
+If you want to contact me you can reach me at [atareao.es](https://www.atareao.es).
+
+## License
+
+This project uses the following license: [MIT License](https://choosealicense.com/licenses/mit/).
---
+++ src/main.py
@@ -0,0 +1,159 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2021 Lorenzo Carbonell <a.k.a. atareao>
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import requests
+from bs4 import BeautifulSoup
+import gi
+try:
+    gi.require_version("Gtk", "3.0")
+    gi.require_version("Gdk", "3.0")
+except ValueError:
+    exit(1)
+from gi.repository import Gtk
+from gi.repository import Gdk
+from asyncf import async_function
+
+
+URL = 'https://dle.rae.es'
+HEADERS = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) \
+            AppleWebKit/537.36 (KHTML, like Gecko) \
+            Chrome/39.0.2171.95 Safari/537.36'}
+
+
+class DictionaryDialog(Gtk.Dialog):
+    def __init__(self):
+        super().__init__()
+        self.set_title("Definiciones")
+        self.clipboard = Gtk.Clipboard.get(Gdk.SELECTION_CLIPBOARD)
+        self.ui()
+        self.show_all()
+
+    def ui(self):
+        grid = Gtk.Grid()
+        grid.set_margin_top(10)
+        grid.set_margin_bottom(10)
+        grid.set_margin_start(10)
+        grid.set_margin_end(10)
+        grid.set_row_spacing(10)
+        grid.set_column_spacing(10)
+        self.get_content_area().add(grid)
+        label = Gtk.Label.new("Palabra:")
+        grid.attach(label, 0, 0, 1, 1)
+        self.entry = Gtk.Entry()
+        grid.attach(self.entry, 1, 0, 1, 1)
+        self.entry.connect("activate", self.on_activate)
+        # self.expander = Gtk.Expander.new("Resultados:")
+        self.expander = Gtk.Expander.new()
+        self.expander.connect("activate", self.on_expander_activate)
+        grid.attach(self.expander, 0, 1, 2, 1)
+
+        scrolledwindow = Gtk.ScrolledWindow()
+        scrolledwindow.set_policy(Gtk.PolicyType.AUTOMATIC,
+                                  Gtk.PolicyType.AUTOMATIC)
+        scrolledwindow.set_shadow_type(Gtk.ShadowType.ETCHED_OUT)
+        scrolledwindow.set_size_request(450,300)
+        self.expander.add(scrolledwindow)
+
+        store = Gtk.ListStore(str)
+        self.resultados = Gtk.TreeView(model=store)
+        self.resultados.connect("row-activated", self.on_row_activated)
+        scrolledwindow.add(self.resultados)
+        self.resultados.append_column(Gtk.TreeViewColumn(
+            'Definiciones', Gtk.CellRendererText(), text=0))
+
+    def on_row_activated(self, treeview, path, _):
+        model = treeview.get_model()
+        value = model.get_value(model.get_iter(path), 0)
+        self.clipboard.set_text(value, -1)
+        print(value)
+
+    def on_expander_activate(self, _):
+        self.resize(1,1)
+
+    def on_activate(self, _):
+        def on_search_done(result, _):
+            if self.expander.get_expanded() is False:
+                self.expander.set_expanded(True)
+            model = self.resultados.get_model()
+            model.clear()
+            for item in result:
+                model.append([item])
+            self.set_normal_cursor()
+
+        @async_function(on_done=on_search_done)
+        def do_search_in_thread():
+            items = []
+            word = self.entry.get_text()
+            if word:
+                word = self.entry.get_text()
+                url = f"{URL}/{word}"
+                response = requests.get(url, headers=HEADERS)
+
+                if response.status_code == 200:
+                    soup = BeautifulSoup(response.text, 'html.parser')
+                    posts = soup.find_all(True,
+                                          {"class":["j", "j1", "j2", "j3",
+                                                    "j4", "j5", "j6", "k2",
+                                                    "k3","k4", "k5", "k6", "m",
+                                                    "l2", "f"]})
+                    if not posts:
+                        print("contecontett")
+                    else:
+                        definition = ""
+                        for post in posts:
+                            checkIfWordStartsNumber = str(post.get_text())
+                            if checkIfWordStartsNumber[0].isdigit():
+                                if definition:
+                                    items.append(definition)
+                                definition = post.get_text()
+                            else:
+                                definition += ". " + post.get_text()
+            return items
+
+        self.set_wait_cursor()
+        do_search_in_thread()
+
+    def set_wait_cursor(self):
+        Gdk.Screen.get_default().get_root_window().set_cursor(
+            Gdk.Cursor(Gdk.CursorType.WATCH))
+        while Gtk.events_pending():
+            Gtk.main_iteration()
+
+    def set_normal_cursor(self):
+        Gdk.Screen.get_default().get_root_window().set_cursor(
+            Gdk.Cursor(Gdk.CursorType.ARROW))
+        while Gtk.events_pending():
+            Gtk.main_iteration()
+
+    def on_realize(self, *_):
+        monitor = Gdk.Display.get_primary_monitor(Gdk.Display.get_default())
+        scale = monitor.get_scale_factor()
+        monitor_width = monitor.get_geometry().width / scale
+        monitor_height = monitor.get_geometry().height / scale
+        width = self.get_preferred_width()[0]
+        height = self.get_preferred_height()[0]
+        self.move((monitor_width - width)/2, (monitor_height - height)/2)
+
+if __name__ == "__main__":
+    dd = DictionaryDialog()
+    dd.run()
